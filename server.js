8d82fb7066eb3f3c4be8100a10ed27600d172cd4
fabianaColombo <fabicriscolombo@gmail.com>
2023-01-09 18:00:59 +0100

    server running first setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, function () {
+  console.log("Server started on port: " + port);
+});
